55478e53c860d5b41232c1a0c6ea0e2c08c3fd92
hi0711 <hiroshi.sato@dmp.co.jp>
2015-02-03 20:14:38 +0900

    done Upgrade

--- cask_upgrade.sh
+++ cask_upgrade.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-for app in $(brew cask list); do # For every app in the Caskroom, do this
-    appToCheck=$(brew cask list "${app}" 2>&1 >/dev/null | grep "${app} is not installed") # If the app is not marked as not installed `brew cask list "app"`, this variable will be "Warning: "app" is not installed",
-
-    if [[ ! -z "${appToCheck}" ]]; then # If the variable is empty, then
-        brew cask install --force "${app}" # Force an install of the app
-    fi
-done
+brew file cask_upgrade -C
