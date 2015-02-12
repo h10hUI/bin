191f38e12db3b172eebf42a52269c58dad9a1795
hi0711 <hiroshi.sato@dmp.co.jp>
2015-02-12 20:45:28 +0900

    modified: dotfileLinks.sh

--- dotfileLinks.sh
+++ dotfileLinks.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.atom ~/.atom
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
+ln -sf ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
