3749b8abf9d524d2cf09d05828091c52f9c12ce8
hi0711 <hiroshi.sato@dmp.co.jp>
2015-02-26 15:21:15 +0900

    add: .jshitrc

--- dotfileLinks.sh
+++ dotfileLinks.sh
@@ -5,3 +5,4 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.atom ~/.atom
 ln -sf ~/dotfiles/.inputrc ~/.inputrc
 ln -sf ~/dotfiles/.gitmessage.txt ~/.gitmessage.txt
+ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
