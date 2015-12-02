28a6f2e684682ab0b844b30667e791998776e2bf
hi0711 <hiroshi.sato@dmp.co.jp>
2015-12-02 12:13:52 +0900

    Modified delete_tmp_file.sh

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # ダウンロードフォルダ1日以上前削除
-find $HOME/Downloads/* -ctime +1 -print0 | xargs -0 rm -rf
+find $HOME/Downloads/* -ctime +1 -print0 | xargs -J% mv % $HOME/.Trash
 # デスクトップフォルダ1日以上前削除
-find $HOME/Desktop/* -ctime +1 -print0 | xargs -0 rm -rf
+find $HOME/Desktop/* -ctime +1 -print0 | xargs -J% mv % $HOME/.Trash 
