f1ac4b3c8c7bfc0992ac8f025220af1773994f2c
hi0711 <h_sato@tam-tam.co.jp>
2016-07-01 13:24:36 +0900

    Updated files

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -6,3 +6,5 @@ find $HOME/Desktop/* -ctime +1 -print0 | xargs -J% mv % $HOME/.Trash
 # vimの不要ファイル削除
 find $HOME/.vim/undo/* -ctime +2 -print0 | xargs  rm -rf
 find $HOME/.vim/backup/* -ctime +2 -print0 | xargs rm -rf
+# .DS_Store削除
+find $Home/ -name "*.DS_Store" | xargs rm -rf
