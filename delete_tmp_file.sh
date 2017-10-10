227176e0cd4418768c452df85d6ab1ebcd72cebe
hi0711 <hiroshi_sato@life-technology.jp>
2017-10-10 20:41:57 +0900

    Updated files

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # ダウンロードフォルダ1日以上前削除
-find $HOME/Downloads/* -ctime +1 -print0 | xargs -J% mv % $HOME/.Trash 2>/dev/null
+find $HOME/Downloads/* -ctime +1 -print0 >/dev/null 2>&1 | xargs -J% mv % $HOME/.Trash
 # デスクトップフォルダ1日以上前削除
-find $HOME/Desktop/* -ctime +1 -print0 | xargs -J% mv % $HOME/.Trash 2>/dev/null
+find $HOME/Desktop/* -ctime +1 -print0 >/dev/null 2>&1 | xargs -J% mv % $HOME/.Trash
 # vimの不要ファイル削除
-find $HOME/.vim/undo/* -ctime +2 -print0 | xargs  rm -rf
-find $HOME/.vim/backup/* -ctime +2 -print0 | xargs rm -rf
+find $HOME/.vim/undo/* -ctime +2 -print0 >/dev/null 2>&1 | xargs  rm -rf
+find $HOME/.vim/backup/* -ctime +2 -print0 >/dev/null 2>&1 | xargs rm -rf
 # .DS_Store削除
 find $HOME -name "*.DS_Store" | xargs rm -rf
--- vimrc_runtime.sh
+++ vimrc_runtime.sh
@@ -1,6 +1,6 @@
 #!bin/sh
-git clone --depth=1 git://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
-mkdir -p ~/.vim/backup
-mkdir -p ~/.vim/swap
-mkdir -p ~/.vim/undo
+git clone --depth=1 git://github.com/amix/vimrc.git $HOME/.vim_runtime
+sh $HOME/.vim_runtime/install_basic_vimrc.sh
+mkdir -p $HOME/.vim/backup
+mkdir -p $HOME/.vim/swap
+mkdir -p $HOME/.vim/undo
