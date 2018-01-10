9d593087a233b65154121604252222c26b6adbe2
hi0711 <hiroshi_sato@life-technology.jp>
2018-01-10 21:38:03 +0900

    Updated files binディレクトリの整理

--- moji.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-for sjis_name in `find .`
-do
-  utf8_name=`echo "${sjis_name}" | iconv -f SJIS -t UTF-8`
-  if [ "${sjis_name}" != "${utf8_name}" ]; then
-    mv "${sjis_name}" "${utf8_name}"
-  fi
-done
