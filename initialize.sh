11938f1988058650794987f2bc4688541f479dfa
hi0711 <hiroshi_sato@life-technology.jp>
2018-01-10 21:52:29 +0900

    Renamed file dotfileLinksをinitializeにリネーム、整理

--- initialize.sh
+++ initialize.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-# ln -sf $HOME/dotfiles/.vimrc $HOME/.vimrc
+
+## Get Brewfile
+curl -L -o $HOME/.brewfile "https://drive.google.com/open?id=0B_yociWy4NahWldUR3FsM0RuSTg"
+
+## dotfile links
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -sf $HOME/dotfiles/.zshenv $HOME/.zshenv
 ln -sf $HOME/dotfiles/.inputrc $HOME/.inputrc
-ln -sf $HOME/dotfiles/.jshintrc $HOME/.jshintrc
 ln -sf $HOME/dotfiles/.lftprc $HOME/.lftprc
-ln -sf $HOME/dotfiles/.ctags $HOME/.ctags
 ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
 ln -sf $HOME/dotfiles/.tigrc $HOME/.tigrc
 ln -sf $HOME/.vim $HOME/.config/nvim
+
+## neovim settings
+mkdir -p $HOME/.config
 ln $HOME/dotfiles/init.vim $HOME/.config/nvim/
-ln $HOME/dotfiles/init.vim $HOME/.vim
-ln $HOME/dotfiles/dein.toml $HOME/.vim
-ln $HOME/dotfiles/dein_lazy.toml $HOME/.vim
+ln $HOME/dotfiles/dein.toml $HOME/.config/nvim
+ln $HOME/dotfiles/dein_lazy.toml $HOME/.config/nvim
