2e9f22f261e17ad365889c42bbf8ca3459c92928
hi0711 <hiroshi_sato@life-technology.jp>
2018-01-13 06:05:49 +0900

    Removed files dotfiles/setup.shへの統合

--- vimrc_runtime.sh
+++
@@ -1,6 +0,0 @@
-#!bin/sh
-git clone --depth=1 git://github.com/amix/vimrc.git $HOME/.vim_runtime
-sh $HOME/.vim_runtime/install_basic_vimrc.sh
-mkdir -p $HOME/.vim/backup
-mkdir -p $HOME/.vim/swap
-mkdir -p $HOME/.vim/undo
