aaab4064caee1609cd301dc6e366731b29d41063
hiroshi <sato@wiredgroup.co.jp>
2019-02-08 13:47:54 +0900

    updated files

--- brew_upgrade.sh
+++ brew_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ~/.zshenv
-brew update; brew upgrade -all
+brew update; brew upgrade
 # brew update; brew upgrade -all; brew cleanup
 brew cask upgrade
 # brew file cask_upgrade -C && brew cask cleanup
