7c882675d64bedd1a9ad58f9738ced863f0f640d
hiroshi <sato@wiredgroup.co.jp>
2019-02-21 20:54:12 +0900

    updated files

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -4,7 +4,7 @@ find ~/Downloads/* -ctime +1 -print0 | xargs -J% mv % ~/.Trash
 # デスクトップフォルダ1日以上前削除
 find ~/Desktop/* -ctime +1 -print0 | xargs -J% mv % ~/.Trash
 # vimの不要ファイル削除
-find ~/.vim/undo/* -ctime +2 -print0 | xargs  rm -rf
-find ~/.vim/backup/* -ctime +2 -print0 | xargs rm -rf
+find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs  rm -rf
+find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs rm -rf
 # .DS_Store削除
 find ~ -not -iwholename "* *" -not -iwholename "*/Gdrive/*" -type f -name "*.DS*" | xargs rm -rf
