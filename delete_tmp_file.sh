2945b3c1a03cb660e348e5c7c2f3676d217fcbc9
hi0711 <admin@hi0711.xyz>
2019-08-15 18:12:46 +0900

    updated files

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -3,6 +3,8 @@
 find ~/Downloads/* -ctime +1 -print0 | xargs -J% mv % ~/.Trash
 # デスクトップフォルダ1日以上前削除
 find ~/Desktop/* -ctime +1 -print0 | xargs -J% mv % ~/.Trash
+# スクリーンショット1日以上前削除
+find ~/Desktop/* -name "スクリーンショット*" -ctime +1 -print0 | xargs rm -rf
 # vimの不要ファイル削除
 find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs  rm -rf
 find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs rm -rf
