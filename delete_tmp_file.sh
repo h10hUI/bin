013e24fde9e96f037bc267862ec64dacc1c5f75d
hi0711 <admin@hi0711.xyz>
2019-10-04 13:56:08 +0900

    updated files

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # ダウンロードフォルダ1日以上前削除
-find ~/Downloads/* -ctime +1 -print0 | xargs -J% mv % ~/.Trash
+find ~/Downloads/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
 # デスクトップフォルダ1日以上前削除
-find ~/Desktop/* -ctime +1 -print0 | xargs -J% mv % ~/.Trash
+find ~/Desktop/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
 # スクリーンショット1日以上前削除
-find ~/Desktop/* -name "スクリーンショット*" -ctime +1 -print0 | xargs rm -rf
+find ~/Desktop/* -name "スクリーンショット*" -ctime +1 -print0 | xargs -0 rm -rf
 # vimの不要ファイル削除
-find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs  rm -rf
-find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs rm -rf
+find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs -0  rm -rf
+find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs -0 rm -rf
 # .DS_Store削除
-find ~ -not -iwholename "* *" -not -iwholename "*/Gdrive/*" -type f -name "*.DS*" | xargs rm -rf
+find ~ -not -iwholename "* *" -not -iwholename "*/Gdrive/*" -type f -name "*.DS*" -print0 | xargs -0 rm -rf
