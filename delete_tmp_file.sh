a52ca72966ccb13451afedab6deb4d8a9640f701
hi0711 <admin@hi0711.xyz>
2019-12-04 13:00:12 +0900

    updated files

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # ダウンロードフォルダ1日以上前削除
 find ~/Downloads/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
 # デスクトップフォルダ1日以上前削除
 find ~/Desktop/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
-# スクリーンショット1日以上前削除
-find ~/Desktop/* -name "スクリーンショット*" -nmin +60 -print0 | xargs -0 rm -rf
+# スクリーンショット90分以上前削除
+find ~/Desktop/* -name "スクリーンショット*" -mmin +90 -print0 | xargs -0 rm -rf
 # vimの不要ファイル削除
 find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs -0  rm -rf
 find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs -0 rm -rf
 # .DS_Store削除
-find ~/Desktop ~/Downloads ~/workspace -type f -name ".DS_*" | xargs rm -rf
+find ~ -type d ! -iwholename "*Gdrive*" -type f -name ".DS_*" -print0 | xargs -0 rm -rf
+
