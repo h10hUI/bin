0e207b4a6fcd1c49204dc54e1a4724e65adc89ef
hiroshi_sato <h.sato@diamondhead.tech>
2019-12-04 18:14:36 +0900

    chore: binディレクトリ整理

--- remove_space.sh
+++ remove_space.sh
@@ -1,3 +1,4 @@
 #!/usr/local/bin/zsh
+
 find $HOME/Desktop -depth -name "* *" -execdir /usr/local/bin/rename 's/ /_/g' "{}" \; >/dev/null 2>&1
 find $HOME/Downloads -depth -name "* *" -execdir /usr/local/bin/rename 's/ /_/g' "{}" \; >/dev/null 2>&1
--- update_ndenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-lts=`ndenv install -l | grep v8. | tail -n 1`
-ndenv install ${lts} >/dev/null 2>&1
-ndenv global ${lts} >/dev/null 2>&1
-npm i -g yarn npm-check-updates
