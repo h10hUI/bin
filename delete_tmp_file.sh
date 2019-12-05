927fa2f30d16ecdf5de26b5885eaa9af6f456a28
hiroshi_sato <h.sato@diamondhead.tech>
2019-12-05 10:57:11 +0900

    delete_tmpがうまく動かないので調整

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -ex
 
 # ダウンロードフォルダ1日以上前削除
-find ~/Downloads/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
+/usr/bin/find ~/Downloads/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
 # デスクトップフォルダ1日以上前削除
-find ~/Desktop/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
+/usr/bin/find ~/Desktop/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
 # スクリーンショット90分以上前削除
-find ~/Desktop/* -name "スクリーンショット*" -mmin +90 -print0 | xargs -0 rm -rf
+/usr/bin/find ~/Desktop/* -name "スクリーンショット*" -mmin +90 -print0 | xargs -0 rm -rf
 # vimの不要ファイル削除
-find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs -0  rm -rf
-find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs -0 rm -rf
+/usr/bin/find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs -0  rm -rf
+/usr/bin/find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs -0 rm -rf
 # .DS_Store削除
-find ~ -type d ! -iwholename "*gdrive*" ! -iwholename "*library*" -type f -name ".DS_*" -print0 | xargs -0 rm -rf
+/usr/bin/find ~/Desktop -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
+/usr/bin/find ~/Downloads -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
+/usr/bin/find ~/workspace -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
 
