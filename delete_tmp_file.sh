35c60edf24fe861d15752f698e477c4cc0c76132
hiroshi_sato <h.sato@diamondhead.tech>
2019-12-06 13:13:47 +0900

    fix: delete_tmp_file修正

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -ex
 
 # ダウンロードフォルダ1日以上前削除
-/usr/bin/find ~/Downloads/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
+/usr/bin/find $HOME/Downloads/* -mmin +1440 -print0 | xargs -0 -J% mv % $HOME/.Trash
 # デスクトップフォルダ1日以上前削除
-/usr/bin/find ~/Desktop/* -ctime +1 -print0 | xargs -0 -J% mv % ~/.Trash
+/usr/bin/find $HOME/Desktop/* -mmin +1440 -print0 | xargs -0 -J% mv % $HOME/.Trash
 # スクリーンショット90分以上前削除
-/usr/bin/find ~/Desktop/* -name "スクリーンショット*" -mmin +90 -print0 | xargs -0 rm -rf
+/usr/bin/find $HOME/Desktop -name "スクリーンショット*" -mmin +90 -print0 | xargs -0 rm -rf
 # vimの不要ファイル削除
-/usr/bin/find ~/.config/nvim/undo/* -ctime +2 -print0 | xargs -0  rm -rf
-/usr/bin/find ~/.config/nvim/backup/* -ctime +2 -print0 | xargs -0 rm -rf
+/usr/bin/find $HOME/.config/nvim/undo -mmin +10080 -print0 | xargs -0  rm -rf
+/usr/bin/find $HOME/.config/nvim/backup -mmin +10080 -print0 | xargs -0 rm -rf
 # .DS_Store削除
-/usr/bin/find ~/Desktop -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
-/usr/bin/find ~/Downloads -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
-/usr/bin/find ~/workspace -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
-/usr/bin/find ~ -maxdepth 1 -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
+/usr/bin/find $HOME/Desktop -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
+/usr/bin/find $HOME/Downloads -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
+/usr/bin/find $HOME/workspace -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
+/usr/bin/find $HOME -maxdepth 1 -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
 
