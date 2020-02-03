5160b4c2b7cce70b1c8e240f557280f8889201b5
hiroshi_sato <h.sato@diamondhead.tech>
2020-02-03 11:33:49 +0900

    updated files

--- delete_tmp_file_ripgrep.sh
+++ delete_tmp_file_ripgrep.sh
@@ -1,3 +1,3 @@
 #!/usr/local/bin/zsh -ex
 
-/usr/local/bin/rg -g '.DS_*' --files --glob=!{Gdrive,Application,Library} /Users/hi0711 | xargs rm -rf
+/usr/local/bin/rg -g '.DS_*' --files --glob=!{Gdrive,Application,Library} /Users/c02w61dthv2h | xargs rm -rf
