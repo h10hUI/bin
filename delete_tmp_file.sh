7ea9d5618c3e86a39c53cb5aac2c2e9fbc8c69d4
hi0711 <admin@hi0711.xyz>
2020-09-18 17:14:47 +0900

    nvim関連コマンド追加

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -7,8 +7,9 @@
 # スクリーンショット90分以上前削除
 /usr/bin/find $HOME/Desktop -name "スクリーンショット*" -mmin +90 -print0 | xargs -0 rm -rf
 # vimの不要ファイル削除
-/usr/bin/find $HOME/.config/nvim/undo -mmin +10080 -print0 | xargs -0  rm -rf
-/usr/bin/find $HOME/.config/nvim/backup -mmin +10080 -print0 | xargs -0 rm -rf
+/usr/bin/find $HOME/.config/nvim/undo -mmin +10080 -print0 | xargs -0  rm -rf && /bin/mkdir $HOME/.config/nvim/undo
+/usr/bin/find $HOME/.config/nvim/backup -mmin +10080 -print0 | xargs -0 rm -rf && /bin/mkdir $HOME/.config/nvim/backup
+/usr/bin/find $HOME/.config/nvim/swap -mmin +10080 -print0 | xargs -0 rm -rf && /bin/mkdir $HOME/.config/nvim/swap
 # .DS_Store削除
 # /usr/bin/find $HOME/Desktop -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
 # /usr/bin/find $HOME/Downloads -type f -iwholename "*ds_store*" -print0 | xargs -0 rm -rf
