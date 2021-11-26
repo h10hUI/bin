bf4b5de73f7e2849631029d9abefbdf675e8dcd1
hi0711 <admin@hi0711.xyz>
2021-11-26 16:08:32 +0900

    config: ⚙️  cron調整

--- brew_bundle.sh
+++ brew_bundle.sh
@@ -1,6 +1,6 @@
 #!/opt/homebrew/bin/zsh -ex
 
 rm -rf $HOME/.Brewfile \
-  && brew bundle dump --global -f \
+  && /opt/homebrew/bin/brew bundle dump --global -f \
   && cp ~/.Brewfile ~/dotfiles/ \
   || :
