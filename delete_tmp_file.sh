ab5d54b3ea2ba338bd8d7b2fe4c372a3c8963f69
h10hUI <admin@hi0711.xyz>
2024-04-01 13:23:01 +0900

    config: ⚙️  update configs

--- delete_tmp_file.sh
+++ delete_tmp_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
 # ダウンロードフォルダ1日以上前削除
-/usr/bin/find $HOME/Downloads/ -mmin +1440 -print0 | xargs -0 -J% mv % $HOME/.Trash
+/usr/bin/find $HOME/Downloads/* -mmin +1440 -print0 | xargs -0 -J% mv % $HOME/.Trash
 # デスクトップフォルダ1日以上前削除
-/usr/bin/find $HOME/Desktop/ -mmin +1440 -print0 | xargs -0 -J% mv % $HOME/.Trash
+/usr/bin/find $HOME/Desktop/* -mmin +1440 -print0 | xargs -0 -J% mv % $HOME/.Trash
 # スクリーンショット90分以上前削除
 /usr/bin/find $HOME/Desktop -name "スクリーンショット*" -mmin +90 -print0 | xargs -0 rm -rf
 # vimの不要ファイル削除
--- delete_tmp_file_ripgrep.sh
+++ delete_tmp_file_ripgrep.sh
@@ -1,3 +1,3 @@
 #!/opt/homebrew/bin/zsh -ex
 
-/opt/homebrew/bin/rg -g '.DS_*' --files --glob=!{Google\ Drive,Application,Library} ${HOME} | xargs rm -rf
+/opt/homebrew/bin/rg -g '.DS_*' --files --glob=\!{Google\ Drive,Applications,Library} ${HOME} | xargs rm -rf
